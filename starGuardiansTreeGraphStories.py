03d703e4a0a2a6a3a8dbf44cf474eff146bea938
Francois Sissoko <fsissoko007@yahoo.com>
2018-08-29 03:38:48 -0600

    Lol(league of legends) Tree Diagram
    Just a design of a game a I am constructing based off of different metrics

---
+++ starGuardiansTreeGraphStories.py
@@ -0,0 +1,39 @@
+#Star Guardians Tree Graph lore
+
+def lobby():
+    pass
+
+def team1():
+    pass
+
+def team2():
+    pass
+
+def invite():
+    pass
+
+def join():
+    pass
+
+def addBot():
+    pass
+
+def makePhraseDictionary():
+    pass
+
+Champs = "LUX"
+luxPhraseBook = ['How did lulu and lux become friends?','lululux','lets do this', \
+'Together fight for Demacia','I have this problem where I cant stop smiling', \
+'The supperior tatic is to never give up', 'Lighting a way', 'never not who you are',\
+'fight the shadows', 'shine bright', 'Keep Believing', 'The thing about light is it never really dies']
+
+
+def main():
+    print(luxPhraseBook[4])
+    print(luxPhraseBook[8])
+    print(luxPhraseBook[11])
+    pass
+
+#Annoying to remember
+if __name__ == '__main__':
+    main()
