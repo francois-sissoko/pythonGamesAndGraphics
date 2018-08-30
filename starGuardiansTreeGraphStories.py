b9d828378653ca27124ef47a4e85c2ffb32e254e
Francois Sissoko <fsissoko007@yahoo.com>
2018-08-30 08:33:06 -0600

    Poppy why tell a joke like that
    Yesterday's Poppy play

--- starGuardiansTreeGraphStories.py
+++ starGuardiansTreeGraphStories.py
@@ -1,6 +1,14 @@
 #Star Guardians Tree Graph lore
 
 def lobby():
+    def update():
+        pass
+        def screenSaver():
+            pass
+        def anime():
+            pass
+        def music():
+            pass
     pass
 
 def team1():
@@ -21,11 +29,20 @@ def addBot():
 def makePhraseDictionary():
     pass
 
-Champs = "LUX"
-luxPhraseBook = ['How did lulu and lux become friends?','lululux','lets do this', \
+
+
+Champs = "LUXPOPPY"
+luxPhraseBook = ['How did lulu and lux become friends?','lululux','lets light it up', \
 'Together fight for Demacia','I have this problem where I cant stop smiling', \
 'The supperior tatic is to never give up', 'Lighting a way', 'never not who you are',\
 'fight the shadows', 'shine bright', 'Keep Believing', 'The thing about light is it never really dies']
+luxweapons = ['wand','star wand','bow','speakers', 'totalrecall','gamma radiation']
+poppyPhraseBook = ['I am no hero just a yordal with a hammar','I am not coming back until I find that hero',\
+'letstrythatagain']
+def makeWeaponStrong(lambda x):
+    x = x / points
+    pass
+
 
 
 def main():
