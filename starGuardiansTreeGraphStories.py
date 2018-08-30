9f7311928df5bb107b808364c751fa578018c16e
Francois Sissoko <fsissoko007@yahoo.com>
2018-08-30 12:57:13 -0600

    Lol(league of legends) Tree Diagram (#6)
    * Lol(league of legends) Tree Diagram  
    Just a design of a game a I am constructing based off of different metrics
    
    * Poppy why tell a joke like that?
    Yesterday's Poppy play

---
+++ starGuardiansTreeGraphStories.py
@@ -0,0 +1,56 @@
+#Star Guardians Tree Graph lore
+
+def lobby():
+    def update():
+        pass
+        def screenSaver():
+            pass
+        def anime():
+            pass
+        def music():
+            pass
+    pass
+
+def team1():
+    pass
+
+def team2():
+    pass
+
+def invite():
+    pass
+
+def join():
+    pass
+
+def addBot():
+    pass
+
+def makePhraseDictionary():
+    pass
+
+
+
+Champs = "LUXPOPPY"
+luxPhraseBook = ['How did lulu and lux become friends?','lululux','lets light it up', \
+'Together fight for Demacia','I have this problem where I cant stop smiling', \
+'The supperior tatic is to never give up', 'Lighting a way', 'never not who you are',\
+'fight the shadows', 'shine bright', 'Keep Believing', 'The thing about light is it never really dies']
+luxweapons = ['wand','star wand','bow','speakers', 'totalrecall','gamma radiation']
+poppyPhraseBook = ['I am no hero just a yordal with a hammar','I am not coming back until I find that hero',\
+'letstrythatagain']
+def makeWeaponStrong(lambda x):
+    x = x / points
+    pass
+
+
+
+def main():
+    print(luxPhraseBook[4])
+    print(luxPhraseBook[8])
+    print(luxPhraseBook[11])
+    pass
+
+#Annoying to remember
+if __name__ == '__main__':
+    main()
