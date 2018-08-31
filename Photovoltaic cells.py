484ace4adfde6b3b78e295c5d5caf543e031a086
Francois Sissoko <fsissoko007@yahoo.com>
2018-08-31 14:37:37 -0600

    photovoltaic_cells
    Gale Cengage Learning 
    3 425-454 3 455
    # 401 3 477-408

---
+++ Photovoltaic cells.py
@@ -0,0 +1,29 @@
+#Photovoltaic cells
+
+key2 = """Negotiations on the transfer of clean energy technologies to developing nations
+continue at the Conference of the Parties (COP16) to the United Nations Framework
+Convention on Climate Change in Cancun, Mexico"""
+key1 = """Kyoto Protocol expires December 13, 2012."""
+key3 = 'Fukushima Daiichi'
+key4 = "Electron cyclotron resonance plasma enhanced sputtering system glows -Semiconductor"
+
+def heading(date):
+    years = [2010,2006,2000]
+    title = "The promise of solar energy: As cheap as fossil fuels?"
+    names = [WalterVarhur,MikeCross]
+
+    solarPower = .1 * date
+    electricityGrowthSolarPower = .12
+    for i in range(5):
+        windBioMass = i * electricityGrowthSolarPower
+    print(windBioMass)
+
+
+
+
+def main():
+    heading(2011)
+    pass
+
+if __name__ == '__main__':
+    main()
