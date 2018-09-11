84180a6ccea65e0fcc2c1d111c6db0c5b0fce0bc
Francois Sissoko <fsissoko007@yahoo.com>
2018-09-10 22:05:42 -0600

    Not master (#8)
    * Lol(league of legends) Tree Diagram  
    
    Just a design of a game a I am constructing based off of different metrics
    
    * Poppy why tell a joke like that
    
    Yesterday's Poppy play
    
    * Awesome Game Star Lux
    
    Losing a game you couldn't out code the other person in
    
    * Update README.md
    
    * photovoltaic_cells
    
    Gale Cengage Learning 
    3 425-454 3 455
    # 401 3 477-408
    
    * Added muteAll specs 
    
    Added Enable Chat
    
    * Calendar Countdown
    
    Get_Events for design of a calendar in python

--- starGuardiansTreeGraphStories.py
+++ starGuardiansTreeGraphStories.py
@@ -1,26 +1,66 @@
 #Star Guardians Tree Graph lore
+import json
+from urllib.request import urlopen
+import time
+#import chat
 
 def lobby():
     def update():
         pass
         def screenSaver():
             pass
-        def anime():
+        def animation():
             pass
-        def music():
+        def music(url):
+            #url = 'https://www.mood.gg/lol'
             pass
+    pass
+
+def enableChat(token):
+    key = "RSApython"
+    key2 = "RSAc"
+    return key, key2
+
+def muteAll():
+    pass
+
+def unmuteAll():
     pass
 
 def team1():
+    enableChat()
+    muteAll()
+    unmuteAll()
     pass
 
 def team2():
-    pass
+    luxweapons()
+    enableChat()
+    muteAll()
 
+def luxweapons():
+	luxPhraseBook
+	luxweapons
+	LUX_WORDS = luxdictionary()
+	print(luxPhraseBook)
+	pass
+
+def luxdictionary():
+    luxTaunts = open('luxcanter.txt')
+    luxLines = {}
+    for line in luxTaunts.read().split('\n'):
+        luxLines[line] = "LUX"
+    luxTaunts.close()
+    return luxLines
+
+#Make server and send message
 def invite():
     pass
 
 def join():
+    pass
+
+def audiofilesofpersonscreaming():
     pass
 
 def addBot():
@@ -29,9 +69,67 @@ def addBot():
 def makePhraseDictionary():
     pass
 
+def makePlayerFeelBad():
+    def changeWallPaperAnimation():
+        exit()
+        pass
+    def changeMusicTone():
+        pass
+    def dictionarySearchingCodex():
+        fail = 'How dare you not save me?'
+        fails = 'yes/sorta'
+        print(fail)
+        audfilesofpersonscreamingio()
+    def lowPriorityQueue(time):
+        window("Low Priority Queue")
+    pass
+
+def window(heading):
+    percentageTop = 10
+    title = length() - top(percentage)
+    width()
+    title = heading
+    pass
+
+def top(value):
+    topScreen = value / 100
+    pass
+
+def length():
+    pass
+
+def width():
+    pass
+
+def makePlayerFeelGood():
+    save = 'Pentakill'
+    linkNA = 'https://www.youtube.com/watch?v=aHjQmgJtjdA&feature=youtu.be'
+    linkOther = 'https://www.youtube.com/watch?v=zomwTVfU4R8'
+    linkOther2 = 'https://youtu.be/zomwTVfU4R8?t=2592'
+    communicate = 'can you talk back to me nicely'
+    lightUpdate()
+    #retrieveYoutubeInfoFeedEntryJSON()
+    pass
+
+def lightUpdate():
+    pass
+
+#O'reilly
+def retrieveYoutubeInfoFeedEntryJSON():
+    response = urlopen(url)
+    contents = response.read()
+    text = contents.decode('utf8')
+    data = json.loads(text)
+    data = json.loads(text)
+    for video in data['feed']['entry'][0:15]:
+        #print(video['title']['$t'])}
+        pass
 
 
-Champs = "LUXPOPPY"
+def animateMotionOfChampion():
+    pass
+
+Champs = "LUXLULUPOPPYJANNA"
 luxPhraseBook = ['How did lulu and lux become friends?','lululux','lets light it up', \
 'Together fight for Demacia','I have this problem where I cant stop smiling', \
 'The supperior tatic is to never give up', 'Lighting a way', 'never not who you are',\
@@ -39,16 +137,27 @@ luxPhraseBook = ['How did lulu and lux become friends?','lululux','lets light it
 luxweapons = ['wand','star wand','bow','speakers', 'totalrecall','gamma radiation']
 poppyPhraseBook = ['I am no hero just a yordal with a hammar','I am not coming back until I find that hero',\
 'letstrythatagain']
-def makeWeaponStrong(lambda x):
+items =['']
+luluPhraseBook = ['Please to meet you!','None of this','Ahhhahaha','shine bright','lets go around again HA']
+jannaPhraseBook = ['Breathless Pace','As you wish','In harmonoy']
+def makeWeaponStrong(x):
+    points = 5
     x = x / points
     pass
 
+def exit(boo):
+    #more to do before I can call it a process like c.pack()
+    process = 'Are you exiting now?'
 
 
 def main():
+    key = 'python'
     print(luxPhraseBook[4])
     print(luxPhraseBook[8])
     print(luxPhraseBook[11])
+    makePlayerFeelGood()
+    print(enableChat(key))
+
     pass
 
 #Annoying to remember
